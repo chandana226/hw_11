653760be2f9809dd96fbb60587ef511108222b55
Chandana Sreya <chandanasreya@Mac.attlocal.net>
2025-04-30 01:23:44 -0700

    Initial commit for FastAPI MLOps

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+model = joblib.load("iris_model.pkl")
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@app.post("/predict")
+def predict(data: IrisInput):
+    input_data = np.array([[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]])
+    prediction = model.predict(input_data)[0]
+    return {"prediction": int(prediction)}
